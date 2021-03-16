2da055f94364237acab738cfa2954773dcb131b6
Yesid <yesid@doublevpartners.com-at-074803668141>
2021-03-16 00:06:49 -0500

    Pantalla home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,9 +8,18 @@ class Home extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            child:Text(
-              "Hola",
+            height: MediaQuery.of(context).size.height * 0.4,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.elliptical(180, 120),
+                bottomRight: Radius.elliptical(180, 120),
+                ),
+              color: Colors.red,
             ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.07,
           ),
         ],
       ),
