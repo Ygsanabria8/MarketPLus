0baf127bbb1fd3af95e7028b421cb59201de445c
Yesid <yesid@doublevpartners.com-at-074803668141>
2021-04-11 23:20:03 -0500

    Página login acorde a diseño

--- lib/pages/logIn.dart
+++ lib/pages/logIn.dart
@@ -14,33 +14,83 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
       body: Container(
-        padding: EdgeInsets.all(15.0),
+        color: Colors.blue[400],
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration:
-                  InputDecoration(labelText: 'Email', icon: Icon(Icons.email)),
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.35,
             ),
-            SizedBox(
-              height: 15.0,
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                  labelText: 'Password', icon: Icon(Icons.vpn_key)),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.65,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50)
+                ),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  Text(
+                    "Iniciar Sesion",
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).textScaleFactor * 30
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                  ),
+                  TextField(
+                    controller: _emailController,
+                    decoration:
+                      InputDecoration(labelText: 'Email'
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password'
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    height: MediaQuery.of(context).size.height * 0.08,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                      ),
+                      disabledColor: Colors.amber,
+                      child: Text(
+                        "Raised Button",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: MediaQuery.of(context).textScaleFactor * 18,
+                        ),  
+                      ),
+                      color: Colors.blue[700],
+                      onPressed: ()  {
+                        print("Hola Raised Button");
+                      },
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
-      ),
+      )
     );
   }
 }
